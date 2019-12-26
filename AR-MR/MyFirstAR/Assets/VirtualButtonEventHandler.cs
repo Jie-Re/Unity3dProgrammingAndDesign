deeef2f0bc803a9d06520e8c6b3e75fca6c06be3
JieRe <1528504591@qq.com>
2019-12-27 03:44:29 +0800

    HW11

---
+++ AR-MR/MyFirstAR/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualButtonEventHandler : MonoBehaviour,IVirtualButtonEventHandler
+{
+    public GameObject vb;
+
+    void Start()
+    {
+        Debug.Log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            Debug.Log("!!!!!!!!!!!!!!!!!!!!!");
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnVirtualButtonPressed()
+    {
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("OnButtonPressed");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Release");
+    }
+
+}
---
+++ AR-MR/VirtualButtonGame/Assets/VBEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VBEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            Debug.Log("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("jump");
+        Debug.Log("OnButtonPressed$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        ani.SetTrigger("idle");
+        Debug.Log("Release######################################################");
+    }
+
+}
